599ac6768a9ae33950dc18435e134d68f8f8bfd2
smartkiddo <njenga05@gmail.com>
2018-12-21 16:13:20 +0300

    initial commit

--- giraffe.rb
+++ giraffe.rb
@@ -1,8 +1,39 @@
-ismale=false
+def match_day(day)
+  day_name=""
 
-if ismale
-  puts "You are male"
+  case day
+  when "mon"
+    day_name="Monday"
+
+  when "tue"
+    day_name="Tuesday"
+
+  when "wed"
+    day_name="Wednesday"
+
+  when "thur"
+    day_name="Thursday"
+
+  when "fri"
+    day_name="Friday"
+
+  when "sat"
+    day_name="Saturday"
+
+  when "sun"
+    day_name="Sunday"
 else
-  puts "You are not male"
+  day_name="Invalid abbreviation"
+  return day_name
+end
+end
 
+puts match_day("mon")
+puts match_day("hello")
+
+index =1
+
+while index <=50
+  puts index
+  index+=1
 end
